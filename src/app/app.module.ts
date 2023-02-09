e33a1daa9bef193295979ab3057b9e328589a241
Hrvoje Perovic <perovic8@gmail.com>
2023-02-09 15:30:41 +0100

    Commit 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [ // ovdje se registriraju nove komponente
+    AppComponent,
+    ServerComponent
   ],
   imports: [
     BrowserModule
